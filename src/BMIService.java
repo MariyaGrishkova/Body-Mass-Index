2d90f36cd59207ecca23bcc7c08f3eda7e4a409f
Mariya Grishkova <galeyevamv@gmail.com>
2020-11-15 10:05:12 +0300

    Update BMIService.java

--- src/BMIService.java
+++ src/BMIService.java
@@ -1,8 +1,8 @@
 public class BMIService {
-    public static double calculate(int service, float growth) {
-        float weight = 99;
+    public static double calculate(int weight, float growth) {
         growth = (float) 1.75;
         double cube = growth * growth;
+        weight = (int) 99;
         int index = (int) (weight / cube);
         return index;
     }
