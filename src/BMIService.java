7adfaeafc4da96dad83ddf659810f585fc804fdf
Mariya Grishkova <galeyevamv@gmail.com>
2020-11-16 19:50:37 +0300

    update BMIService

--- src/BMIService.java
+++ src/BMIService.java
@@ -1,9 +1,6 @@
 public class BMIService {
     public static double calculate(int weight, float growth) {
-        growth = (float) 1.75;
-        double cube = growth * growth;
-        weight = (int) 99;
-        int index = (int) (weight / cube);
+       double index = weight / (growth*growth);
         return index;
     }
 
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         BMIService service = new BMIService();
-        int weight = 99;
+        int weight = 88;
         float growth = (float) 1.75;
         double index = service.calculate(weight, growth);
         System.out.println(index);
